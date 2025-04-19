93a2e8f385dcd28476dc44b934e7ea6f4c014e55
Hamza Yaseen <hamzza.yaseen@gmail.com>
2025-04-19 22:58:46 +0500

    2nd commit

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
+      // useNewUrlParser: true,
       // useUnifiedTopology: true,
     });
     const dbName = mongoose.connection.name;
